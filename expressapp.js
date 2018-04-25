887406c97d1e65013568c54fd7b70fd495f2071a
Andy Evans <andrew.evans@metoffice.gov.uk>
2018-04-25 08:18:43 +0100

    code cleanup

--- expressapp.js
+++ expressapp.js
@@ -1,39 +1,39 @@
-var express = require('express')
-    , app = express()
-    , sse = require('./sse')
+var express = require('express'),
+    app = express(),
+    sse = require('./sse');
 
-var connections = []
-    , votes = {yes: 0, no: 0}
+var connections = [],
+    votes = {yes: 0, no: 0};
 
-app.engine('jade', require('jade').__express) //__
-app.set('view engine', 'jade')
+app.engine('jade', require('jade').__express);
+app.set('view engine', 'jade');
 
-app.use(sse)
+app.use(sse);
 
 app.get('/', function(req, res) {
     res.render('vote')
-})
+});
 
 app.get('/result', function(req, res) {
     res.render('result')
-})
+});
 
 app.get('/vote', function(req, res) {
-    if (req.query.yes === "true") votes.yes++
-    else votes.no++
+    if (req.query.yes === "true") votes.yes++;
+    else votes.no++;
 
     for(var i = 0; i < connections.length; i++) {
-        connections[i].sseSend(votes)
+        connections[i].sseSend(votes);
     }
 
-    res.sendStatus(200)
-})
+    res.sendStatus(200);
+});
 
 app.get('/stream', function(req, res) {
-    res.sseSetup()
-    res.sseSend(votes)
-    connections.push(res)
-})
+    res.sseSetup();
+    res.sseSend(votes);
+    connections.push(res);
+});
 
 app.listen(3000, function() {
     console.log('Listening on port 3000...')
