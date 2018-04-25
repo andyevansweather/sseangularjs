f361b62a46f8bac5ddb5744e3ac565310edfb0e5
Andy Evans <andrew.evans@metoffice.gov.uk>
2018-04-25 08:21:13 +0100

    added cors to express app

--- sse.js
+++ sse.js
@@ -3,13 +3,15 @@ module.exports = function (req, res, next) {
         res.writeHead(200, {
             'Content-Type': 'text/event-stream',
             'Cache-Control': 'no-cache',
-            'Connection': 'keep-alive'
+            'Connection': 'keep-alive',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
         })
-    }
+    };
 
     res.sseSend = function(data) {
         res.write("data: " + JSON.stringify(data) + "\n\n");
-    }
+    };
 
     next()
-}
+};
