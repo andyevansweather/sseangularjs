5e0e4eba1fc27dcb8ad2e63a1d324155656722dd
Andy Evans <andrew.evans@metoffice.gov.uk>
2018-04-26 10:53:42 +0100

    added mousedown and mouseup

--- app.js
+++ app.js
@@ -5,12 +5,35 @@ angular
     .module('app')
     .controller('MainController', MainController);
 
-MainController.$inject = ['$scope'];
+MainController.$inject = ['$scope', '$http'];
 
-function MainController($scope) {
+function MainController($scope, $http) {
     var es = new EventSource("http://localhost:8080/");
 
     $scope.test = '';
+    $scope.output = '';
+
+    function getData() {
+        return $http.get('http://localhost:3000/stuff');
+    }
+
+    function getMoreData() {
+        return $http.get('http://localhost:3000/stufftwo');
+    }
+
+    function activate() {
+        return getData().then(function (resp) {
+            console.log('what is the response?');
+            console.log(resp);
+        })
+    }
+
+    function activate2() {
+        return getData().then(function (resp) {
+            console.log('what is the response?');
+            console.log(resp);
+        })
+    }
 
     es.onmessage = function (event) {
         $scope.$emit('event-start');
@@ -21,6 +44,9 @@ function MainController($scope) {
     $scope.customfunction = function () {
         $scope.$emit('event-start');
     }
+
+    $scope.getMoreData = activate;
+    $scope.getMoreDataTwo = activate2;
 
     $scope.$on('event-start', function (event, args) {
         console.log('I have been sent!');
--- expressapp.js
+++ expressapp.js
@@ -14,6 +14,24 @@ app.get('/', function(req, res) {
     res.render('vote')
 });
 
+app.get('/stuff', function(req, res) {
+    let thing = {
+        test: 'things'
+    };
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.send(JSON.stringify(thing))
+});
+
+app.get('/stufftwo', function(req, res) {
+    let thing = {
+        test: 'things'
+    };
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.send(JSON.stringify(thing))
+});
+
 app.get('/result', function(req, res) {
     res.render('result')
 });
